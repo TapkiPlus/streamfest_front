5e1b286881ddafa44ba4dfee43196928f78bb059
skiboorg <d@skib.org>
2021-01-26 20:12:25 +0300

    new pages

---
+++ components/StreamerCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="$router.push(`/streamers/${name_slug}`)" class="streamers-item">
+    <img :src="image" alt="">
+    <p class="streamers-item__nick" >{{nickname}}</p>
+    <p class="streamers-item__fio">{{name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['image','nickname','name','name_slug'],
+  data() {
+    return {}
+  },
+
+}
+</script>
+
+
