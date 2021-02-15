fac20f9cc9132383fc48882eb0e538857a1c73e5
dmitry-worker <dzenmassta@gmail.com>
2021-02-15 12:51:15 +0300

    Fixed errors in layout, added subscriber methods

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-import serveStatic from 'serve-static'
+import serveStatic from "serve-static";
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   //  server: {
@@ -10,45 +10,41 @@ export default {
   //   },
 
   env: {
-    img_url: 'http://localhost:8000',
+    img_url: "http://localhost:8000",
   },
-  serverMiddleware: [
-    '~/middleware/seo.js'
-  ],
+  serverMiddleware: ["~/middleware/seo.js"],
   head: {
-    title: 'streamfest_front',
+    title: "streamfest_front",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,600&display=swap' },
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,600&display=swap",
+      },
+    ],
   },
-  css: [
-    'element-ui/lib/theme-chalk/index.css',
-    '@/assets/main.sass',
-  ],
+  css: ["element-ui/lib/theme-chalk/index.css", "@/assets/main.sass"],
   plugins: [
-    '@/plugins/element-ui',
-    '@/plugins/scroll',
-    { src: '@/plugins/swiper', mode: 'client' },
+    "@/plugins/element-ui",
+    "@/plugins/scroll",
+    { src: "@/plugins/swiper", mode: "client" },
+    { src: "@/plugins/lightbox", mode: "client" },
   ],
   components: true,
-  buildModules: [
-  ],
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/auth',
-  ],
+  buildModules: [],
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth"],
   axios: {
     // baseURL:'http://185.119.57.46:8000'
-    baseURL:'http://localhost:8000'
+    baseURL: "http://localhost:8000",
   },
   build: {
     transpile: [/^element-ui/],
@@ -57,12 +53,12 @@ export default {
     strategies: {
       local: {
         endpoints: {
-          login: { url: '/', method: 'post', propertyName: 'auth_token' },
-          logout: { url: '/', method: 'post' },
-          user: { url: '/', method: 'get', propertyName: false }
+          login: { url: "/", method: "post", propertyName: "auth_token" },
+          logout: { url: "/", method: "post" },
+          user: { url: "/", method: "get", propertyName: false },
         },
-        tokenType: 'Token'
-      }
-    }
-  }
-}
+        tokenType: "Token",
+      },
+    },
+  },
+};
