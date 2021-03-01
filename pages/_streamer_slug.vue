651780a4f36c460b3bf29dd7a081b984c1c54998
RA360 <github360@gmail.com>
2021-03-01 15:53:04 +0400

    fixies-12

--- pages/_streamer_slug.vue
+++ pages/_streamer_slug.vue
@@ -2,21 +2,37 @@
   <section class="streamer">
     <div class="container ">
       <div class="streamer-top">
-        <div class="streamer-top__avatar" :style="`background: url(${streamer.photo})`"></div>
-        <div class="streamer-top__big" :style="`background: url(${streamer.pageHeader})`"></div>
-
+        <div
+          class="streamer-top__avatar"
+          :style="`background: url(${streamer.photo})`"
+        ></div>
+        <div
+          class="streamer-top__big"
+          :style="`background: url(${streamer.pageHeader})`"
+        ></div>
       </div>
       <div class="streamer-container">
         <div class="streamer-info">
           <div class="streamer-info__name">
-            <p>{{streamer.nickName}}</p>
-            <p>{{streamer.name}}</p>
+            <p>{{ streamer.nickName }}</p>
+            <p>{{ streamer.name }}</p>
           </div>
           <div class="streamer-info__links">
-            <a :href="`https://${social_link.link}`" target="_blank"
-               v-for="social_link in streamer.links"
-               :key="social_link.id">
-              <img class="streamer-info__icon" :src="social_link.icon.icon" alt="">
+            <a
+              :href="
+                social_link.link.startsWith('http')
+                  ? social_link.link
+                  : `https://${social_link.link}`
+              "
+              target="_blank"
+              v-for="social_link in streamer.links.slice().reverse()"
+              :key="social_link.id"
+            >
+              <img
+                class="streamer-info__icon"
+                :src="social_link.icon.icon"
+                alt=""
+              />
             </a>
           </div>
         </div>
@@ -26,19 +42,30 @@
         </div>
         <div class="streamer-bottom">
           <div class="streamer-bottom__buttons">
-<!--            @click.prevent="addItem(ticket.id,streamer.id)" -->
-            <div v-for="ticket in tickets" :key="ticket.id" class="streamer-bottom__button "
-                 :class="[ticket.is_one_day ? 'streamer-bottom__button--yellow' : 'streamer-bottom__button--red']">
-              <p>скоро {{ticket.is_one_day ? 'билет на 1 день' : 'билет на 2 дня'}}</p>
-              <p>от {{streamer.nickName}}</p>
+            <!--            @click.prevent="addItem(ticket.id,streamer.id)" -->
+            <div
+              v-for="ticket in tickets"
+              :key="ticket.id"
+              class="streamer-bottom__button "
+              :class="[
+                ticket.is_one_day
+                  ? 'streamer-bottom__button--yellow'
+                  : 'streamer-bottom__button--red'
+              ]"
+            >
+              <p>
+                скоро
+                {{ ticket.is_one_day ? "билет на 1 день" : "билет на 2 дня" }}
+              </p>
+              <p>от {{ streamer.nickName }}</p>
             </div>
           </div>
-          <p class="streamer-bottom__text">Покупая билет от участника, ты его поддерживаешь.
-            Часть стоимости билета идет участнику.</p>
+          <p class="streamer-bottom__text">
+            Покупая билет от участника, ты его поддерживаешь. Часть стоимости
+            билета идет участнику.
+          </p>
         </div>
       </div>
-
-
     </div>
   </section>
 </template>
@@ -47,40 +74,41 @@
 export default {
   scrollToTop: true,
   // auth: true,
-  async asyncData({$axios,params}){
-    const get_streamer = await $axios.get(`/api/get_streamer?name_slug=${params.streamer_slug}`)
-    const streamer = get_streamer.data
-    const get_tickets = await $axios.get(`/api/get_tickets`)
-    const tickets = get_tickets.data
-    return {streamer,tickets}
+  async asyncData({ $axios, params }) {
+    try {
+      const get_streamer = await $axios.get(
+        `/api/get_streamer?name_slug=${params.streamer_slug}`
+      );
+      const streamer = get_streamer.data;
+      const get_tickets = await $axios.get(`/api/get_tickets`);
+      const tickets = get_tickets.data;
+      return { streamer, tickets };
+    } catch (error) {
+      throw { statusCode: 404, message: "Streamer not found" };
+    }
   },
   data() {
-    return {
-    };
+    return {};
   },
-  watch: {
-  },
-  mounted() {
-  },
+  watch: {},
+  mounted() {},
   methods: {
-    notify(title,message,type){
+    notify(title, message, type) {
       this.$notify({
         title: title,
         message: message,
         type: type
       });
     },
-    async addItem(t_id,s_id){
-     await this.$axios.post('/api/add_item',{
-       session_id:this.$auth.$storage.getCookie('session_id'),
-       item_id:t_id,
-       streamer_id:s_id
-     })
-     this.notify('Успешно','Билет добавлен в корзину', 'success')
-     await this.$store.dispatch('cart/fetchCart')
-   }
+    async addItem(t_id, s_id) {
+      await this.$axios.post("/api/add_item", {
+        session_id: this.$auth.$storage.getCookie("session_id"),
+        item_id: t_id,
+        streamer_id: s_id
+      });
+      this.notify("Успешно", "Билет добавлен в корзину", "success");
+      await this.$store.dispatch("cart/fetchCart");
+    }
   }
-}
+};
 </script>
-
-
