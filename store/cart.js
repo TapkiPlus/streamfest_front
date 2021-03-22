c73ccd1153f90b39eca0381108b6e6eabc5823af
RA360 <github360@gmail.com>
2021-03-23 02:32:54 +0400

    changed code

--- store/cart.js
+++ store/cart.js
@@ -17,9 +17,11 @@ export default {
           "SET_DATA",
           (await this.$axios.get(`/api/get_cart?session_id=${session_id}`)).data
         );
-        console.log((await this.$axios.get(`/api/get_cart?session_id=${session_id}`)).data);
+      console.log(
+        (await this.$axios.get(`/api/get_cart?session_id=${session_id}`)).data
+      );
     },
-    async addItem({ state, dispatch }, { t_id, s_id = 0 }) {
+    async addItem({ dispatch }, { t_id, s_id = 0 }) {
       await this.$axios.post("/api/add_item", {
         session_id: this.$auth.$storage.getCookie("session_id"),
         item_id: t_id,
@@ -27,15 +29,17 @@ export default {
       });
       dispatch("fetchCart");
     },
-    async changeQuantity({ state, dispatch }, { t_id, quantity }) {
-      await this.$axios.post("/api/change_item_quantity", {
-        session_id: this.$auth.$storage.getCookie("session_id"),
-        item_id: t_id,
-        quantity
-      });
+    async changeQuantity({ dispatch }, { t_id, increase }) {
+      await this.$axios.post(
+        `/api/${increase ? "add_item_quantity" : "delete_item_quantity"}`,
+        {
+          session_id: this.$auth.$storage.getCookie("session_id"),
+          item_id: t_id
+        }
+      );
       dispatch("fetchCart");
     },
-    async deleteItem({ state, dispatch }, { t_id }) {
+    async deleteItem({ dispatch }, { t_id }) {
       await this.$axios.post("/api/delete_item", {
         session_id: this.$auth.$storage.getCookie("session_id"),
         item_id: t_id
