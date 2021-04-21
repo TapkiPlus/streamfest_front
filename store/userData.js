798829c2a504c55e4ce51848b414e88fb55b1d36
RA360 <github360@gmail.com>
2021-04-22 01:09:26 +0400

    added prevent sending faster saveData POST

--- store/userData.js
+++ store/userData.js
@@ -1,10 +1,23 @@
 export default {
+  state() {
+    return {
+      disabledSending: false
+    };
+  },
+  mutations: {
+    DISABLE_SENDING(state, payload) {
+      state.disabledSending = payload;
+    }
+  },
   actions: {
-    saveData(_, obj) {
+    saveData({ state, commit }, obj) {
+      if (state.disabledSending) return;
       this.$axios.post("/api/save_user_data", {
         session_id: this.$auth.$storage.getCookie("session_id"),
         ...obj
       });
+      commit("DISABLE_SENDING", true);
+      setTimeout(() => commit("DISABLE_SENDING", false), 500);
     }
   }
 };
