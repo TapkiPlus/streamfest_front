f5f50b7a93812d65dda9929ac0ed508c851ac21b
RA360 <raibrahim360@gmail.com>
2021-07-31 00:34:26 +0400

    added question-answer in faq and changed text in contact

--- pages/faq.vue
+++ pages/faq.vue
@@ -10,8 +10,8 @@
       </p>
       <div class="faq-items">
         <button
-          @click="toggleAccordion(1)"
-          :class="{ 'faq-item-active': accordionIds.includes(1) }"
+          @click="toggleAccordion(0)"
+          :class="{ 'faq-item-active': accordionIds.includes(0) }"
           class="faq-item"
         >
           <svg class="faq-icon" width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -25,7 +25,7 @@
           </svg>
         </button>
         <div
-          :class="{ visible: accordionIds.includes(1) }"
+          :class="{ visible: accordionIds.includes(0) }"
           class="faq-item__inner"
         >
           <p>
@@ -38,6 +38,35 @@
             > от 22 июня 2021 г. №35-УМ.
           </p>
           <p>Стримфест проходит в формате COVID-free, потому что он позволяет делать фестиваль безопасно и «как раньше»: без ограничений, без масок, перчаток и страха, зато с обнимашками, фудкортами и ресторанами.</p>
+        </div>
+        <button
+          @click="toggleAccordion(1)"
+          :class="{ 'faq-item-active': accordionIds.includes(1) }"
+          class="faq-item"
+        >
+          <svg class="faq-icon" width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M19.3304 9.67198L16.4195 0.617977C16.2556 0.109311 15.6756 -0.0566894 15.3072 0.300644L12.6853 2.84598C11.7143 2.07398 8.12269 2.07398 7.15173 2.84598L4.52979 0.300644C4.1614 -0.0566894 3.58076 0.109311 3.41753 0.617977L0.506566 9.67198C-0.0889176 11.5246 0.672373 13.59 2.29173 14.4233C4.33753 15.476 6.92399 15.9673 9.91882 15.9673C12.913 15.9673 15.4995 15.476 17.5453 14.4233C19.1646 13.59 19.9259 11.5246 19.3304 9.67198Z" fill="#0060CF"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M19.3304 9.67198L16.4195 0.617977C16.2556 0.109311 15.6756 -0.0566894 15.3072 0.300644L12.6853 2.84598C11.7143 2.07398 8.12269 2.07398 7.15173 2.84598L4.52979 0.300644C4.1614 -0.0566894 3.58076 0.109311 3.41753 0.617977L0.506566 9.67198C-0.0889176 11.5246 0.672373 13.59 2.29173 14.4233C4.33753 15.476 6.92399 15.9673 9.91882 15.9673C12.913 15.9673 15.4995 15.476 17.5453 14.4233C19.1646 13.59 19.9259 11.5246 19.3304 9.67198Z" fill="#C4C4C4"/>
+          </svg>
+          Разве QR-коды уже не отменили?
+          <svg class="faq-plus" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="14" y="5" width="2" height="20" rx="1" fill="#BAC8CF"/>
+            <rect x="25" y="14" width="2" height="20" rx="1" transform="rotate(90 25 14)" fill="#BAC8CF"/>
+          </svg>
+        </button>
+        <div
+          :class="{ visible: accordionIds.includes(1) }"
+          class="faq-item__inner"
+        >
+          <p>
+            QR-коды никто не отменял. Их просто сделали необязательными для кафе и ресторанов, а некоторые СМИ подали эту новость как полную отмену. На самом деле, это не так. Согласно <a
+              href="https://www.mos.ru/authority/documents/doc/46784220/"
+              class="link"
+              target="_blank"
+              rel="noopener noreferrer"
+              >п. 33.4 указа Мэра Москвы</a
+            > № 42-УМ от 16.07.2021, вход на массовые мероприятия по-прежнему только по QR-коду. Это безопасно! И, похоже, новая норма для фестивалей и концертов.
+          </p>
         </div>
         <button
           @click="toggleAccordion(2)"
