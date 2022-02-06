53af430d6cfbc99619d33386bca81a89ae89fe2c
RA360 <raibrahim360@gmail.com>
2022-02-06 16:56:50 +0300

    added rendering response from create order api to give user ability to enter card info

--- store/checkout.js
+++ store/checkout.js
@@ -1,18 +1,20 @@
 export default {
   actions: {
     async getPayLink(_, { firstname, lastname, email, phone }) {
-        try {
-          const { data } = await this.$axios.post("/api/create_order", {
-            session_id: this.$auth.$storage.getCookie("session_id"),
-            firstname,
-            lastname,
-            email,
-            phone
-          });
-            window.location.href = data;
-        } catch (e) {
-          return false
-        }
+      try {
+        const { data } = await this.$axios.post("/api/create_order", {
+          session_id: this.$auth.$storage.getCookie("session_id"),
+          firstname,
+          lastname,
+          email,
+          phone
+        });
+
+        document.write(data);
+        document.close();
+      } catch (e) {
+        return false;
+      }
     }
   }
 };
