f52d92047e8094f2f8c70fd5083d4847c16f5274
Dmitry Voronov <dmitry.voronov@iohk.io>
2022-02-09 08:15:46 +0300

    No more serve-static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,4 @@
-import serveStatic from "serve-static";
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  // target: "static",
   server: {
     port: 3000,
     host: "localhost"
