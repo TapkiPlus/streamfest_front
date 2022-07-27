c6337b14ddbfdd85473eea71c745674e78c3f82b
Dmitry Voronov <dmitry.voronov@iohk.io>
2022-07-27 13:20:20 +0400

    Twitch player on main

--- pages/index.vue
+++ pages/index.vue
@@ -30,20 +30,15 @@
           лучшие выходные года!
         </p>
         <div style="padding: 56.25% 0 0 0; position: relative">
-          <iframe
-            src="https://player.vimeo.com/video/530958754?title=0&byline=0&portrait=0"
-            loading="lazy"
-            style="
+          <iframe src="https://player.twitch.tv/?channel=cartoonclub69&parent=sf.voronov.at&autoplay=true&muted=true"
+            frameborder="0" style="
               position: absolute;
               top: 0;
               left: 0;
               width: 100%;
               height: 100%;
-            "
-            frameborder="0"
-            allow="autoplay; fullscreen; picture-in-picture"
-            allowfullscreen
-          ></iframe>
+            " allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
+          </iframe>
         </div>
         <div class="video-wrapper">
           <!--
